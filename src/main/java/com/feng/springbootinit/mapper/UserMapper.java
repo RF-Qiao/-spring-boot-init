f88efd2b4533f9be4bd7424cf98beb0bd8b1d3b8
RF-Qiao <1328678738@qq.com>
2023-03-05 23:00:38 +0800

    feng

---
+++ src/main/java/com/feng/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.feng.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.feng.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
